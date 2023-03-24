bd950f24becf97d7116a672f249dc581c29d1929
paul <pkavule@gmail.com>
2023-03-24 16:07:45 +0300

    first commit

---
+++ ExtApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ExtApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+           
+        }
+
+        [HttpGet("/{nin}")]
+        public IActionResult GetDetails(string nin)
+        {
+            try
+            {
+                var client = new ThdApi.ThirdPartyInterfaceFacadeNewClient();
+                var request = new ThdApi.getPerson { request = new ThdApi.getPersonRequest { nationalId = nin } };
+                var result = client.getPersonAsync(request);
+
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { ErrorCode = 100, ErrorDescription = ex.Message });
+            }
+            
+        }
+    }
+}
---
+++ Tester/Program.cs
@@ -0,0 +1,29 @@
+﻿
+var request = "<soapenv:Envelopexmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:fac=\"http://facade.server.pilatus.thirdparty.tidis.muehlbauer.de/\""
++"xmlns:wsse= \"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\">"
++"<soapenv:Header>"
++ "<wsse:UsernameToken>"
++ "<wsse:Username>Test@ROOT</wsse:Username>"
++ "<wsse:Password Type=\"PasswordDigest\">ZMvfJzFWcWkrWGd10gz7wYVY/js=</wsse:Password>"
++"<wsse:Nonce>aYdz/Rbe/laaPKl1qPdaPQ==</wsse:Nonce>"
++"<wsse:Created>2018-04-06T19:32:31.543+03:00</wsse:Created>"
++"</wsse:UsernameToken>"
++"</soapenv:Header>"
++"<soapenv::Body>"
++"<!–- SOAP Body data -->"
++"</soapenv:Body>"
++"</soapenv:Envelope>";
+try
+{
+    var client = new HttpClient();
+    var content = new StringContent(request, System.Text.Encoding.UTF8, "text/xml");
+    var postResp = await client.PostAsync("", content);
+    if (postResp.IsSuccessStatusCode)
+        Console.WriteLine("Error: " + postResp.StatusCode);
+    var resultStr = await postResp.Content.ReadAsStringAsync();
+    Console.WriteLine("Hurray: "+ resultStr);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
